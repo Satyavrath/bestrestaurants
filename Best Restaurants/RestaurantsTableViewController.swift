6ec669f5d99e7aabe0e2982d1b4ffc3af6020bef
Injamuri,Satyavrath <s533983@ch140026m.nwmissouri.edu>
2019-03-05 10:57:49 -0600

    created add button

--- Best Restaurants/RestaurantsTableViewController.swift
+++ Best Restaurants/RestaurantsTableViewController.swift
@@ -52,15 +52,19 @@ class RestaurantsTableViewController: UITableViewController {
 //    }
     
     
-    
+    override func viewWillAppear(_ animated: Bool) {
+        tableView.reloadData()
+    }
     // MARK: - Navigation
     
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
+        
+        if segue.identifier == "restaurantCell" {
         let restaurantDetailsVC = segue.destination as! ResturantDetailsViewController
         restaurantDetailsVC.restaurant = Restaurants.shared[tableView.indexPathForSelectedRow!.row]
     }
     
-    
+    }
 }
